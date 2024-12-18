79998ce8f02883341cc4ed499f0745789e93a7b5
letoast <ciroman@gmail.com>
2024-12-18 13:49:44 +0100

    fix date

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,7 +1,7 @@
 // https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: 'server',
-  compatibilityDate: '2024-12-18',
+  compatibilityDate: '2024-04-01',
   preset: 'netlify_builder',
   routeRules: {
     '/api/**': { cache: false, swr: false },
